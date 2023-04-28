6fcffb863341b6524168e055036366d2d0ba50a1
Ismael Amezcua <amezcua.ismael@gmail.com>
2023-04-28 15:12:07 -0600

    feat: add MantineProvider and Supabase's SessionContextProvider in _app file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,40 +1,29 @@
-/*
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { MantineProvider } from '@mantine/core'
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-    <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{colorScheme: 'light'}}
-      >
-    <Component {...pageProps} />
-    </MantineProvider>
-    </>
-  )
-}
-*/
-import { useState } from 'react'
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
-import { AppProps } from 'next/app'
+import { useState } from "react";
+import type { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
 
-function MyApp({
+export default function App({
   Component,
   pageProps,
-}: AppProps<{
-  initialSession: Session,
-}>) {
-  const [supabase] = useState(() => createBrowserSupabaseClient())
+}: AppProps<{ initialSession: Session }>) {
+  const [supabase] = useState(() => createBrowserSupabaseClient());
 
   return (
-    <SessionContextProvider 
-    supabaseClient={supabase} 
-    initialSession={pageProps.initialSession}>
-      <Component {...pageProps} />
-    </SessionContextProvider>
-  )
+    <>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{ colorScheme: "light" }}
+      >
+        <SessionContextProvider
+          supabaseClient={supabase}
+          initialSession={pageProps.initialSession}
+        >
+          <Component {...pageProps} />
+        </SessionContextProvider>
+      </MantineProvider>
+    </>
+  );
 }
-export default MyApp
